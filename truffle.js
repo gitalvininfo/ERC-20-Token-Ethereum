3d45b536aa572225dc3e1725984704cd72094dc1
Alvin <yanson.alvin.info@gmail.com>
2021-09-01 18:15:46 +0800

    done with token sale

--- truffle.js
+++ truffle.js
@@ -13,5 +13,10 @@ module.exports = {
       network_id: 4,
       gas: 4700000
     }
-  }
+  },
+  compilers: {
+    solc: {
+      version: ">=0.4.24", // Fetch exact version from solc-bin (default: truffle's version)
+    }
+  },
 };
